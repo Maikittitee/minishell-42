0a62b447a6832e82c906536bf445e8568ac0a700
Kittinon Saelim <ksaelim@z3t1c3.42bangkok.com>
2023-05-29 17:18:45 +0700

    save1

---
+++ minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksaelim <ksaelim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 15:16:54 by ksaelim           #+#    #+#             */
+/*   Updated: 2023/05/28 15:19:36 by ksaelim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "stdio.h"
+# include "readline/readline.h"
+# include "readline/history.h"
+
+#endif
