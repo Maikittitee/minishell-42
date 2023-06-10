a176d02190b62ce65d1c7ddc5385ec0ad0ea37f8
ktunchar <maikittitee1615@gmail.com>
2023-06-10 22:44:23 +0700

    hello mark

---
+++ exe.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av, char **env)
+{
+
+
+
+	char *arg[3];
+
+	arg[0] = "echo";
+	arg[1] = getenv("ZSH"); // -> use when try to find buildin variable
+	arg[2] = NULL;
+
+	execve("/bin/echo", arg, env);
+}
