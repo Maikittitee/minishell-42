ebff97b1c95cd049878b0bbff9bfd4087c72a6fb
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-06-24 01:13:23 +0700

    test

--- test.c
+++ test.c
@@ -4,8 +4,19 @@ int	main()
 {
 	int	fd[2];
 	int	pid;
+	int	x;
 
 	pipe(fd);
 	printf("IN PIPE is %d\n", fd[0]);
 	printf("OUT PIPE is %d\n", fd[1]);
+
+	pid = fork();
+	if (pid == 0)
+	{
+		read(fd[1], &x, 4);
+		printf("hello from child 1 and x is %d\n", x);
+
+	}
+	// x = 6;
+	// write(fd[0], &x, 4);
 }
