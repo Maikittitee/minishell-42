1a4bf3bff3579e9c98dba7966c02c30f64395d4b
Kittinon Saelim <ksaelim@z3t5c4.42bangkok.com>
2023-07-10 17:31:35 +0700

    07.10.23

--- ft_tokenize.c
+++ ft_tokenize.c
@@ -1,16 +1,13 @@
 #include "../minishell.h"
 
-// int valid_arg(char *arg)
-// {
-// 	(void)arg;
-// 	return (0);
-// }
 static char	*keep_token(char **s)
 {
 	char *token;
 	int	len;
 
-	len = word_len(s);
+	while (**s && **s == ' ')
+		(*s)++;
+	len = word_len(*s);
 	token = malloc(sizeof(char) * len + 1);
 	if (!token)
 		return (NULL);
@@ -19,13 +16,13 @@ static char	*keep_token(char **s)
 	return (token);
 }
 
-int ft_tokenize(char *s, t_token *token)
+int	ft_tokenize(char *s, t_shell *shell)
 {
 	int		i;
 
 	if (!s)
 		return (0);
-	token->value = (char **)malloc((token->n_token + 1) * sizeof(char *));
+	token->value = malloc((token->n_token + 1) * sizeof(char *));
 	if (!token->value)
 		return (0);
 	i = -1;
--- valid_arg.c
+++ valid_arg.c
@@ -6,52 +6,44 @@
 /*   By: ksaelim <ksaelim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 11:48:59 by ksaelim           #+#    #+#             */
-/*   Updated: 2023/06/28 12:11:05 by ksaelim          ###   ########.fr       */
+/*   Updated: 2023/07/10 17:20:14 by ksaelim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	word_len(char **s)
-{	
+int	word_len(char *s)
+{
 	int	len;
 
 	len = 0;
-	while (**s && **s == ' ')
-		(*s)++;
-	if (ft_isrdir(**s))
-		len = skip_rdir(*s);
-	else if (**s == '$')
+	if (ft_isrdir(*s) || *s == PIPE)
+		len = skip_rdir(s);
+	else if (*s == '$')
 		len = 1;
 	else
-	{
-		while (s[0][len] && ft_isarg(s[0][len]))
-		{
-			if (ft_isquote(s[0][len]))
-				len += skip_qoute(*s, **s);
-			else
-				len++;
-		}
-	}
-	if (!len)
-		return (0);
-	return (len);
+		len = skip_arg(s);
+	if (len)
+		return (len);
+	return (0);
 }
 
-int	valid_arg(char *s)
+int	valid_arg(char *s, int *n_token)
 {
-	int n_token;
 	int	valid;
 
-	n_token = 0;
+	valid = 0;
 	while (*s)
 	{
-		valid = word_len(&s);
+		while (*s && *s == ' ')
+			s++;
+		if (!(*s))
+			return (*n_token);
+		valid = word_len(s);
 		if (!valid)
 			return (0);
-		n_token++;
 		s += valid;
+		*n_token += 1;
 	}
-	printf("yeahhhh\n");
-	return (n_token);
+	return (*n_token);
 }
