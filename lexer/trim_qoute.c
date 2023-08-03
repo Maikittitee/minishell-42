f3e76fcc8dcf04e8a3a5d0b10a17848c12de74de
Kittinon Saelim <ksaelim@z2t17c4.42bangkok.com>
2023-08-03 22:38:45 +0700

    clean trim

--- lexer/trim_qoute.c
+++ lexer/trim_qoute.c
@@ -6,48 +6,49 @@
 /*   By: ksaelim <ksaelim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 14:42:29 by ksaelim           #+#    #+#             */
-/*   Updated: 2023/08/02 21:12:51 by ksaelim          ###   ########.fr       */
+/*   Updated: 2023/08/03 22:27:06 by ksaelim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
-void	trim_quote(t_token **token)
+static char	*trim_out(char *str, int quote)
 {
 	int		i;
 	int		q;
 	char	*trimed;
-	char	*str;
+	char	*tmp;
+
+	i = 0;
+	tmp = str;
+	trimed = malloc(sizeof(char) * (ft_strlen(str) - (quote * 2) + 1));
+	while (*str)
+	{
+		if (*str && ft_isquote(*str))
+		{
+			q = skip_qoute(str, *str, NULL) - 2;
+			str++;
+			while (q--)
+				trimed[i++] = *(str)++;
+			str++;
+		}
+		else
+			trimed[i++] = *(str)++;
+	}
+	trimed[i] = '\0';
+	free(tmp);
+	return (trimed);
+}
+
+void	trim_quote(t_token **token)
+{
 	t_token	*tmp;
 
 	tmp = *token;
 	while (tmp)
 	{
 		if (tmp->quote)
-		{
-			i = 0;
-			str = tmp->content;
-			trimed = malloc(sizeof(char) * (ft_strlen(str) - (tmp->quote * 2) + 1));
-			while (*str)
-			{
-				if (*str && ft_isquote(*str))
-				{
-					q = skip_qoute(str, *str, NULL) - 2;
-					str++;
-					while (q--)
-						trimed[i++] = *(str)++;
-					str++;
-				}
-				else
-					trimed[i++] = *(str)++;
-			}
-			trimed[i] = '\0';
-			free(tmp->content);
-			tmp->content = ft_strdup(trimed);
-			free(trimed);
-			tmp->quote--;
-		}
+			tmp->content = trim_out(tmp->content, tmp->quote);
 		tmp = tmp->next;
 	}
 }
